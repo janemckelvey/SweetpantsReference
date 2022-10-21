a7aed07eeb26eff51c94e595ae60fff02312b16e
Dennis Marro <dennismarro@earthlink.net>
2022-10-21 12:37:03 -0700

    Add trans

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,16 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final class PneumaticIDs {
+    //Solenoids
+    public static final int kDrivetrainShiftSolenoid = 0;
+    public static final int kRampSolenoid = 1;
+    public static final int kClimberSolenoid = 2;
+
+    // public static final int kDrivetrainShiftSolenoidLow = 0;
+    // public static final int kDrivetrainShiftSolenoidHigh = 1;
+    // public static final int kRampSolenoidOpen = 2;
+    // public static final int kRampSolenoidClosed = 3;
+   
+}}
